9375cc379b8649d4137a40ef35f27adfba63353f
Edouard George <edouard@edge.pf>
2016-02-22 03:40:32 +0000

    add the vertical lest menu

--- views/menu/menu.php
+++ views/menu/menu.php
@@ -9,4 +9,21 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
     </button>
+    <!--left bar-->
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+	            <li class="sidebar-search">
+		            <div class="input-group custom-search-form">
+			            <input type="text" class="form-control" placeholder="Recherche...">
+			            <span class="input-group-btn">
+				            <button class="btn btn-default" type="button">
+					            <i class="fa fa-search"></i>
+				            </button>
+			            </span>
+		            </div>
+	            </li>
+	       </ul>
+        </div>
+    </div>
 </nav>
